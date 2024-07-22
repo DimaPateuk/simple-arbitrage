ce42183bbe50f18180e1deb11f16668a9e1fc17b
Dima pateuk <dimapateuk@gmail.com>
2024-07-22 12:34:46 +0300

    try to fix service script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-source ${HOME}/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 cd ~/simple-arbitrage
 npm run start
