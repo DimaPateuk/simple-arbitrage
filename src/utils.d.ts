181c789bb864211cecb6260568a0e0b94ede81bd
Dima pateuk <dimapateuk@gmail.com>
2024-07-22 11:36:45 +0300

    trying make it work on remote machine

--- src/utils.d.ts
+++
@@ -1,4 +0,0 @@
-import { BigNumber } from "ethers";
-export declare const ETHER: BigNumber;
-export declare function bigNumberToDecimal(value: BigNumber, base?: number): number;
-export declare function getDefaultRelaySigningKey(): string;
